ca5b51ff82f12a588932144242780fa703d3c976
khosyrobbin <khosybin@gmail.com>
2022-11-13 23:31:57 +0700

    update seeder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,15 @@ class DatabaseSeeder extends Seeder
             ['name' => 'Admin1',
             'email' => 'admin1@gmail.com',
             'password' => bcrypt('123456789'),
-            'level' => '1',]
+            'level' => '1',],
+            ['name' => 'Admin3',
+            'email' => 'admin3@gmail.com',
+            'password' => bcrypt('123456789'),
+            'level' => '1',],
+            ['name' => 'Admin2',
+            'email' => 'admin2@gmail.com',
+            'password' => bcrypt('123456789'),
+            'level' => '1',],
         ];
         DB::table('users')->insert($user);
     }
